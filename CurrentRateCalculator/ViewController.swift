56d7b72e4f8df97e42733f3de79b16812a36d561
EllGHT <rmsgh1188@gmail.com>
2025-04-15 15:26:20 +0900

    feat: 테이블 뷰 UI

--- CurrentRateCalculator/ViewController.swift
+++ CurrentRateCalculator/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // 테이블 뷰에다가 테이블 뷰 셀 등록
+        tableView.register(RatesCell.self, forCellReuseIdentifier: RatesCell.id)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(#function)
+        
+        setUI()
+        setLayout()
     }
-
+    
+    private func setUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(tableView)
+    }
+    
+    private func setLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RatesCell.id) as? RatesCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}

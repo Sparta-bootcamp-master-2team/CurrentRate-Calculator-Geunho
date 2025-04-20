f429326d1457dc7adfc2504015eca02ec8f9e416
EllGHT <rmsgh1188@gmail.com>
2025-04-20 16:30:28 +0900

    feat: CoreData 설정 및 파일 생성

---
+++ ExchangeRateCalculator/Data/CoreDataManager.swift
@@ -0,0 +1,64 @@
+//
+//  CoreDataManager.swift
+//  ExchangeRateCalculator
+//
+//  Created by 정근호 on 4/20/25.
+//
+
+import CoreData
+import UIKit
+
+class CoreDataManager {
+    
+    var container: NSPersistentContainer!
+    
+    init(container: NSPersistentContainer!) {
+        self.container = container
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.container = appDelegate.persistentContainer
+    }
+    
+    func createData(currencyCode: String) {
+        guard let entity = NSEntityDescription.entity(forEntityName: Favorites.className, in: self.container.viewContext) else { return }
+        let newFavorites = NSManagedObject(entity: entity, insertInto: self.container.viewContext)
+        newFavorites.setValue(currencyCode, forKey: Favorites.Key.currencyCode)
+        
+        do {
+            try self.container.viewContext.save()
+            print("저장 성공")
+        } catch {
+            print("저장 실패")
+        }
+    }
+    
+    func readAllData() {
+        do {
+            let favorites = try self.container.viewContext.fetch(Favorites.fetchRequest())
+            for favorite in favorites as [NSManagedObject]{
+                if let currencyCode = favorite.value(forKey: Favorites.Key.currencyCode) as? String {
+                    print("currencyCode in CoreData: \(currencyCode)")
+                }
+            }
+        } catch {
+            print("읽기 실패")
+        }
+    }
+    
+    func deleteData(selectedCode: String) {
+        let fetchRequest = Favorites.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "currencyCode == %@", selectedCode)
+        do {
+            let result = try self.container.viewContext.fetch(fetchRequest)
+            
+            for data in result as [NSManagedObject] {
+                self.container.viewContext.delete(data)
+            }
+            try self.container.viewContext.save()
+            print("삭제 성공")
+        } catch {
+            print("삭제 실패")
+        }
+    }
+    
+}
